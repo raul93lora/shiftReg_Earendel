bbe0e1e558894384686394bfabf3e5aee01968a1
Raúl Lora <raul.lora@ontech.local>
2025-03-13 09:33:16 +0100

    Quito generador de onda cuadrada y counter asociado

--- generator.v
+++ generator.v
@@ -22,8 +22,8 @@ module generator(
 	input wire signal_in;
 	input wire SELDYN;
 	input wire SELSTAT;
+	wire signal_aux;
 	output reg signal_out;
-	reg [23:0] counter;   // Contador para dividir la frecuencia
 	wire [SIZESRDYN-1:0] DYN_in;     // Valor dinámico 
 	wire [SIZESRSTAT-1:0] STAT_in;   // Valor estático
 	reg [SIZESRDYN-1:0] REGDYN;      // Registro dinámico 
@@ -37,42 +37,35 @@ module generator(
 	if(!RST_N) 
 		REGDYN <= DYN_in;
 		REGSTAT <= STAT_in;
-	else if(SELDYN == 1'b1) begin
+	end else if(SELDYN == 1'b1) begin
 		REGDYN <= {REGDYN[SIZESRDYN-2:0,1'b0]};	// Desplazo a la izquierda y por la derecha relleno con ceros
-	else if(SELSTAT == 1'b1) begin
+       end else if(SELSTAT == 1'b1) begin
 		REGSTAT <= {REGSTAT[SIZESRSTAT-2:0,1'b0]};	// Desplazo a la izquierda y por la derecha relleno con ceros
-	end
-	end
-
-	// Generador de señal cuadrada
-	always @(posedge CLK or negedge RST_N) 
-	begin
-	if (!RST_N) begin
-		counter <= 0;       // Reset: reinicia el contador
-		signal_out <= 0;    // Reset: fuerza la salida a 0
-	end else begin
-		counter <= counter + 1;
-		if (counter == 8_000_000) begin // Ajusta para cambiar la frecuencia
-			signal_out <= ~signal_out; // Invierte la señal
-			counter <= 0;
-		end
 	end
 	end
 
 	// Output Mux
 	always @ (*) begin
 	case({SELDYN,SELSTAT})
-		2'b00: signal_out <= 1'b0;
-		2'b01: signal_out <= REGDYN[SIZESRDYN-1];
-		2'b10: signal_out <= REGSTAT[SIZESRSTAT-1];
-		2'b11: signal_out <= 1'b0;
-		default: signal_out <= 1'b0;
+		2'b00: signal_aux <= 1'b0;
+		2'b01: signal_aux <= REGDYN[SIZESRDYN-1];
+		2'b10: signal_aux <= REGSTAT[SIZESRSTAT-1];
+		2'b11: signal_aux <= 1'b0;
+		default: signal_aux <= 1'b0;
 	endcase
 	end
 
+	// Asignar salida a signal_out
+	always @(posedge CLK or negedge RST_N) begin
+	if(!RST_N) 
+		signal_out <= 1'b0;
+	else
+		signal_out <= signal_aux;
+	end
+
 	// Mapping registers dynamic and static
-	assign DYNIN[SIZESRDYN-1:0] = 16'hABCD;
-	assign REGSTAT[SIZESRSTAT-1:0] = 88'h123456789ABCDEF1234567;
+	assign DYN_in[SIZESRDYN-1:0] = 16'hABCD;
+	assign STAT_in[SIZESRSTAT-1:0] = 88'h123456789ABCDEF1234567;
 
 endmodule
 
